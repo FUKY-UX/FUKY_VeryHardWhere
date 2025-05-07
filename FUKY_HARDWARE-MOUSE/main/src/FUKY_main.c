780102d7322f044b12574c4238713235e97dc0a5
Radish-FUKY <3994362575@qq.com>
2025-05-08 00:29:02 +0800

    最后还是用回了单核心，更稳定

--- FUKY_HARDWARE-MOUSE/main/src/FUKY_main.c
+++ FUKY_HARDWARE-MOUSE/main/src/FUKY_main.c
@@ -5,122 +5,24 @@
 #include "bno080.h"
 #include "ble_hidd.h"
 #include "esp_timer.h"
-//共享数据结构
-typedef struct 
-{
-    bool is_floating;
-    SemaphoreHandle_t mutex;  // 互斥锁句柄
 
-        // 鼠标传感器数据
-        int8_t raw_x;
-        int8_t raw_y;
-        
-        // IMU传感器数据
-        float lin_accel_x;
-        float lin_accel_y;
-        float lin_accel_z;
-} 
-SharedState_t;
 //static const char TAG[] = "FUKYMOUSE";
 
-//======================================初始化====================================//
+//======================================SPI====================================//
 #define MOSI    6
 #define MISO    3
 #define SCLK    4
 #define FUKY_SPI_HOST    SPI2_HOST
 
-SharedState_t shared_state = 
-{
-    .is_floating = false,
-    .mutex = NULL,
-    .raw_x = 0,
-    .raw_y = 0,
-    .lin_accel_x = 0.0f,
-    .lin_accel_y = 0.0f,
-    .lin_accel_z = 0.0f,
-};
-
-//int16_t delta_x, delta_y;
-
-bool IsFloating = false;
+int16_t delta_x, delta_y;
+IMUData_t IMUDataBuffer;
 
 void Main_Init(void);
 void Mouse_Function(void);
 
-void IMUtask_core0(void *pvParameters) 
-{
-    IMUData_t local_imu_data;
-    SharedState_t *state = (SharedState_t *)pvParameters;
-    while (1) 
-    {
-        local_imu_data = bno080_Function();
-        // 带锁访问共享状态
-        if (xSemaphoreTake(state->mutex, portMAX_DELAY)) 
-        {
-            state->lin_accel_x = local_imu_data.lin_accel_x;
-            state->lin_accel_y = local_imu_data.lin_accel_y;
-            state->lin_accel_z = local_imu_data.lin_accel_z;
-            if (state->is_floating) 
-            {
-                SendIMUData(local_imu_data.lin_accel_x, local_imu_data.lin_accel_y, 
-                    local_imu_data.lin_accel_z, local_imu_data.quat_i, 
-                    local_imu_data.quat_j, local_imu_data.quat_k, 
-                    local_imu_data.quat_w);
-            }
-            xSemaphoreGive(state->mutex);
-        }
-    }
-
-}
-
-void MOUSEtask_core1(void *pvParameters) 
-{
-    SharedState_t *state = (SharedState_t *)pvParameters;
-    int8_t local_raw_x = 0, local_raw_y = 0;
-
-    while (1)
-    {
-        PAW3805_Function(&local_raw_x, &local_raw_y);
-        // 带锁访问共享状态
-        if (xSemaphoreTake(state->mutex, portMAX_DELAY)) 
-        {
-            state->raw_x = local_raw_x;
-            state->raw_y = local_raw_y;
-            if (!state->is_floating) 
-            {
-                send_mouse_value(0, local_raw_x, local_raw_y);
-            }
-            xSemaphoreGive(state->mutex);
-        }
-    }
-}
-
-void Main_Function(void *pvParameters)
-{
-    SharedState_t *state = (SharedState_t *)pvParameters;
-    while(1)
-    {
-        if(xSemaphoreTake(state->mutex, portMAX_DELAY))
-        {
-            bool new_floating =((state->raw_x == 0 && state->raw_y == 0) && (state->lin_accel_z > 0.05 || state->lin_accel_y > 0.05 || state->lin_accel_x > 0.05));
-            state->is_floating = new_floating;
-            xSemaphoreGive(state->mutex);
-        }
-
-    }
-}
-
 void app_main(void)
 {
-
-        // 初始化互斥锁
-    shared_state.mutex = xSemaphoreCreateMutex();
-    if (shared_state.mutex == NULL) 
-    {
-        printf("Failed to create mutex!\n");
-        return;
-    }
-    printf("创建互斥锁\n");
+    int8_t raw_x = 0, raw_y = 0;  // 初始化
     // 初始化SPI总线
     Main_Init();
     //蓝牙初始化
@@ -131,15 +33,24 @@ void app_main(void)
     init_paw3805ek(FUKY_SPI_HOST);
     //printf("初始化BNO080...\n");
     bno080_init(FUKY_SPI_HOST);
+    
+    while (1)
+    {
 
-    // 将task_core0绑定到core0，增加堆栈大小以避免溢出
-    xTaskCreatePinnedToCore(IMUtask_core0, "IMUtask_core0", 4096, &shared_state, 5, NULL, 0);
-    // 将task_core1绑定到core1
-    xTaskCreatePinnedToCore(MOUSEtask_core1, "MOUSEtask_core1", 2048, &shared_state, 5, NULL, 1);
-    // 控制蓝牙发送数据类型
-    xTaskCreatePinnedToCore(Main_Function, "Main_Function", 2048, &shared_state, 3, NULL, 1);
+        PAW3805_Function(&raw_x,&raw_y);
+        bool Isfloating = (raw_x == 0 && raw_x == 0);
+        if(Isfloating)
+        {
+            IMUDataBuffer = bno080_Function();
+            SendIMUData(IMUDataBuffer.lin_accel_x,IMUDataBuffer.lin_accel_y,IMUDataBuffer.lin_accel_z,
+                IMUDataBuffer.quat_i,IMUDataBuffer.quat_j,IMUDataBuffer.quat_k,IMUDataBuffer.quat_w);
+                continue;
+        }
+        send_mouse_value(0,raw_x,raw_y);
 
+    }
 }
+
 
 
 
