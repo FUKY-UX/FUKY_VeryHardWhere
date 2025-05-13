86c5da1f53b4173e1ee87b4cf2107c66c5212c9b
Radish-FUKY <3994362575@qq.com>
2025-05-13 22:50:18 +0800

    回退了光电的频率，稳定性很重要

--- FUKY_HARDWARE-MOUSE/main/src/FUKY_main.c
+++ FUKY_HARDWARE-MOUSE/main/src/FUKY_main.c
@@ -192,12 +192,13 @@ void MouseTask(void *pvParameters)
         if(local_IsMouseFloating)
         {
             SendButtonState(button_state);
-            vTaskDelay(pdMS_TO_TICKS(1));        //过频繁地访问光电会导致芯片重启和读数异常,因为burst不可用，目前回报率提不上来
+            //vTaskDelay(pdMS_TO_TICKS(1));        //过频繁地访问光电会导致芯片重启和读数异常,因为burst不可用，目前回报率提不上来
+            esp_rom_delay_us(1500);
             continue;
         }
         send_mouse_value(button_state,local_raw_x,local_raw_y);
-        vTaskDelay(pdMS_TO_TICKS(1));        //过频繁地访问光电会导致芯片重启和读数异常,因为burst不可用，目前回报率提不上来
-
+        //vTaskDelay(pdMS_TO_TICKS(1));        //过频繁地访问光电会导致芯片重启和读数异常,因为burst不可用，目前回报率提不上来
+        esp_rom_delay_us(1500);
         // 正常就只用HID发送按钮
     }
         
